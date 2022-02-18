b7b3abf8f5c9dc55d8327ab1000bcf11b3ad259c
Ishaan <ishaanb04@gmail.com>
2022-02-17 21:35:28 -0500

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Ishaan Bhasin on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
